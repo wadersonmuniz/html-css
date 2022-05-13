50a0dffcd81ae1c7a236f8134bcdcd6fc3ccc815
wadersonmuniz <60373998+wadersonmuniz@users.noreply.github.com>
2022-05-12 22:44:27 -0300

    criar pasta exercicios
    criar pasta exercicios

---
+++ exercicios/ex002/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parágrafos</title>
+</head>
+<body>
+    <h1>Parágrafos e quebras de linha</h1>
+    <hr>
+    <p>Você pode escrever um parágrafo de qualquer jeito, basta colocar entre tags -p</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br> Quas ad perferendis ducimus facilis est voluptates autem maxime in, unde, sed omnis illo praesentium? Asperiores dolor architecto officia fuga sunt commodi.</p>
+
+    <p>Vamos adcionar símbolos</p>
+    &reg;
+    &copy;
+    
+    <p>Adiconar emoji</p>
+    <!-- buscar emojipedia -->
+    &#x1F596;
+    &#x1F913;
+</body>
+</html>
